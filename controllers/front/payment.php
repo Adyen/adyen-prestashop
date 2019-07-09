f0254db52f05be8b736c1563c63fa69c6e664abe
alessio <alessio.zampatti@adyen.com>
2019-07-09 18:09:11 +0200

    PW-1364(1.6 + 1.7):Set order status as paid(2), save card details and pspreference so that it is visible on backend

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -46,12 +46,36 @@ class AdyenPaymentModuleFrontController extends ModuleFrontController
         $this->helper_data->adyenLogger()->logDebug($response);
 
         if ($this->helper_data->isPrestashop16()) {
-            $this->setTemplate('after.tpl');
+            $this->setTemplate('payment_return.tpl');
         } else {
-            $this->setTemplate('module:adyen/views/templates/front/after.tpl');
+            $this->setTemplate('module:adyen/views/templates/front/payment_return.tpl');
         }
-        //todo: check second argument(order status)
-        $this->module->validateOrder($cart->id, 1, $cart->getOrderTotal());
+
+        $customer = new Customer($cart->id_customer);
+        if (!Validate::isLoadedObject($customer)) {
+            Tools::redirect('index.php?controller=order&step=1');
+        }
+
+        $currency = $this->context->currency;
+        $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
+        $extra_vars = array(
+            'transaction_id' => $response['pspReference']
+        );
+
+        $this->module->validateOrder($cart->id, 2, $total, $this->module->displayName, null, $extra_vars,
+            (int)$currency->id, false, $customer->secure_key);
+        $new_order = new Order((int)$this->module->currentOrder);
+        if (Validate::isLoadedObject($new_order)) {
+            $payment = $new_order->getOrderPaymentCollection();
+            if (isset($payment[0])) {
+                //todo add !empty
+                $payment[0]->card_number = pSQL($response['additionalData']['cardBin'] . " *** " . $response['additionalData']['cardSummary']);
+                $payment[0]->card_brand = pSQL($response['additionalData']['paymentMethod']);
+                $payment[0]->card_expiration = pSQL($response['additionalData']['expiryDate']);
+                $payment[0]->card_holder = pSQL($response['additionalData']['cardHolderName']);
+                $payment[0]->save();
+            }
+        }
         Tools::redirect('index.php?controller=order-confirmation&id_cart=' . $cart->id . '&id_module=' . $this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key);
         return $response;
     }
