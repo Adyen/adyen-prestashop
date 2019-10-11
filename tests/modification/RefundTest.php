4492681f063809dba46636fb64d9f891459d12cb
marcoss <marcos.silvagarcia@adyen.com>
2019-10-11 16:12:49 +0200

    Fix Refund tests

--- tests/modification/RefundTest.php
+++ tests/modification/RefundTest.php
@@ -23,15 +23,17 @@
 namespace Adyen\PrestaShop\service\modification;
 
 use Adyen\Service\Modification;
+use OrderSlip;
 use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
 
 class RefundTest extends TestCase
 {
     public function refundDataProvider()
     {
         return [
-            ['1234567890abcdef', '123', 'EUR', '1', 'PrestaShopTest', '1-refund'],
-            ['abcdef1234567890', '100', 'BRL', '2', 'PrestaShopTest', '2-refund']
+            ['1234567890abcdef', '123', 'EUR', '1', 'PrestaShopTest', '1'],
+            ['abcdef1234567890', '100', 'BRL', '2', 'PrestaShopTest', '1']
         ];
     }
 
@@ -54,7 +56,7 @@ class RefundTest extends TestCase
         $merchantAccount,
         $merchantReference
     ) {
-        /** @var \PHPUnit_Framework_MockObject_MockObject|Modification $modificationClient */
+        /** @var PHPUnit_Framework_MockObject_MockObject|Modification $modificationClient */
         $modificationClient = $this->getMockBuilder('Adyen\Service\Modification')
             ->disableOriginalConstructor()
             ->getMock();
@@ -63,27 +65,35 @@ class RefundTest extends TestCase
             ->with($this->equalTo([
                 'originalReference' => $pspReference,
                 'modificationAmount' => [
-                    'value' => $amount,
+                    'value' => $amount * 100,
                     'currency' => $currency
                 ],
-                'reference' => $merchantReference,
+                'reference' => (string)$merchantReference,
                 'merchantAccount' => $merchantAccount
             ]))
             ->willReturn(true);
 
-        $pattern = 'select * from ps_adyen_notification a inner join ps_orders o on a.merchant_reference = o.id_order';
+        /** @var PHPUnit_Framework_MockObject_MockObject|OrderSlip $orderSlip */
+        $orderSlip = $this->getMockBuilder('OrderSlip')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $orderSlip->id_order = $orderId;
+        $orderSlip->amount = $amount;
+        $orderSlip->id = 1;
+
+        $pattern = 'select * from ps_adyen_notification a inner join ps_orders o on a.merchant_reference = o.id_cart';
         $pattern = str_replace('*', '.+', $pattern);
         $pattern = str_replace(' ', '\\s+', $pattern);
-        /** @var \PHPUnit_Framework_MockObject_MockObject|\Db $databaseConnection */
+        /** @var PHPUnit_Framework_MockObject_MockObject|\Db $databaseConnection */
         $databaseConnection = $this->getMockBuilder('Db')
             ->disableOriginalConstructor()
             ->getMock();
         $databaseConnection->expects($this->once())
-            ->method('query')
-            ->with($this->matchesRegularExpression("/$pattern/si"))
-            ->willReturn([['pspReference' => $pspReference, 'orderId' => $orderId]]);
+                           ->method('executeS')
+                           ->with($this->matchesRegularExpression("/$pattern/si"))
+                           ->willReturn([['pspReference' => $pspReference]]);
 
         $refund = new Refund($modificationClient, $databaseConnection, $merchantAccount);
-        $this->assertEquals(true, $refund->request($amount, $currency));
+        $this->assertEquals(true, $refund->request($orderSlip, $currency));
     }
 }
