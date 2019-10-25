6ef46178f4c4e7fac0d941b47b335b5f3daa9f23
marcoss <marcos.silvagarcia@adyen.com>
2019-10-25 13:11:56 +0200

    Add a wrapper for ServiceLocator

---
+++ service/adapter/classes/ServiceLocator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen PrestaShop plugin
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+namespace Adyen\PrestaShop\service\adapter\classes;
+
+use PrestaShop\PrestaShop\Adapter\CoreException;
+
+class ServiceLocator
+{
+    /**
+     * @param string $serviceName
+     *
+     * @return mixed|object
+     * @throws CoreException
+     */
+    public static function get($serviceName)
+    {
+        if (class_exists('Adapter_ServiceLocator')) {
+            return \Adapter_ServiceLocator::get($serviceName);
+        } else {
+            return \PrestaShop\PrestaShop\Adapter\ServiceLocator::get($serviceName);
+        }
+    }
+}
