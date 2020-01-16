bc4ef7a2c1339f329938664f5c0009fc2dcf2bd3
attilak <attila.kiss@adyen.com>
2020-01-16 11:53:00 +0100

    Add State adapter class
    Add getIsoById() to retrieve the state ISO code by the state id

---
+++ service/adapter/classes/State.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen PrestaShop plugin
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+namespace Adyen\PrestaShop\service\adapter\classes;
+
+class State
+{
+    /**
+     * Retrieves Iso code for a state by id
+     *
+     * @param $id_state
+     * @return bool
+     */
+    public function getIsoById($id_state)
+    {
+        if (!$id_state) {
+            return false;
+        }
+
+        $cache_id = 'State::getIsoById_' . (int)$id_state;
+        if (!\Cache::isStored($cache_id)) {
+            $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+				SELECT `iso_code`
+				FROM `' . _DB_PREFIX_ . 'state`
+				WHERE `id_state` = ' . (int)$id_state
+            );
+
+            \Cache::store($cache_id, $result);
+            return $result;
+        }
+
+        return \Cache::retrieve($cache_id);
+    }
+}
