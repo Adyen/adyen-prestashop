d720a025439cc449a876f24b588c7c38bb8dcfd8
attilak <attila.kiss@adyen.com>
2020-01-22 14:49:53 +0100

    Remove the not necessary fields from the checkout components
    The open invoice components like afterpay renders a lot of not necessary
    fields for the user to get input from them. We already have most of the
    information such as first name, last name etc in the invoicing address
    To make the checkout easier leave only the 'date of birth', 'gender' and
    'telephone number' fields to show

--- views/js/payment-components/local-payment-method.js
+++ views/js/payment-components/local-payment-method.js
@@ -19,6 +19,47 @@
  * See the LICENSE file for more info.
  */
 
+/**
+ * In the open invoice components we need to validate only the personal details and only the
+ * dateOfBirth, telephoneNumber and gender if it's set in the admin
+ * @param details
+ * @returns {Array}
+ */
+function filterOutOpenInvoiceComponentDetails(details) {
+    var filteredDetails = details.map(function (parentDetail) {
+        if ("personalDetails" === parentDetail.key) {
+            var detailObject = parentDetail.details.map(function (detail) {
+                if ('dateOfBirth' === detail.key  ||
+                    'telephoneNumber' === detail.key  ||
+                    'gender' === detail.key) {
+                    return detail;
+                }
+            });
+
+            if (!!detailObject) {
+                return {
+                    "key": parentDetail.key,
+                    "type": parentDetail.type,
+                    "details": self.filterUndefinedItemsInArray(detailObject)
+                };
+            }
+        }
+    });
+
+    return filterUndefinedItemsInArray(filteredDetails);
+}
+
+/**
+ * Helper function to filter out the undefined items from an array
+ * @param arr
+ * @returns {*}
+ */
+function filterUndefinedItemsInArray(arr) {
+    return arr.filter(function (item) {
+        return typeof item !== 'undefined';
+    });
+}
+
 jQuery(function ($) {
     if (!window.adyenCheckout) {
         return;
@@ -41,6 +82,9 @@ jQuery(function ($) {
         if (!paymentMethodContainer.length) {
             return;
         }
+
+        // filter personal details extra fields from component and only leave the necessary ones
+        paymentMethod.details = filterOutOpenInvoiceComponentDetails(paymentMethod.details);
 
         /*Use the storedPaymentMethod object and the custom onChange function as the configuration object together*/
         var configuration = Object.assign(paymentMethod, {
