17767e1365984ebb3ca3249b7fc0d9d25f0597ea
marcoss <marcos.silvagarcia@adyen.com>
2020-02-14 11:37:05 +0100

    Remove `break` statements after `return` statements
    `break` is never reached and PrestaShop complains about it.

--- controllers/front/Validate3d.php
+++ controllers/front/Validate3d.php
@@ -70,7 +70,9 @@ class AdyenValidate3dModuleFrontController extends \Adyen\PrestaShop\controllers
             $service = ServiceLocator::get('Adyen\PrestaShop\service\Checkout');
             $response = $service->paymentsDetails($request);
         } catch (AdyenException $e) {
-            $this->logger->error("Error during validate3d paymentsDetails call: exception: " . $e->getMessage());
+            $this->logger->error(
+                "Error during validate3d paymentsDetails call: exception: " . $e->getMessage()
+            );
             $this->ajaxRender(
                 $this->helperData->buildControllerResponseJson(
                     'error', array('message' => "Something went wrong. Please choose another payment method.")
@@ -88,8 +90,17 @@ class AdyenValidate3dModuleFrontController extends \Adyen\PrestaShop\controllers
         }
         switch ($resultCode) {
             case 'Authorised':
-                $this->module->validateOrder($cart->id, 2, $total, $this->module->displayName, null, $extra_vars,
-                    (int)$currency->id, false, $customer->secure_key);
+                $this->module->validateOrder(
+                    $cart->id,
+                    2,
+                    $total,
+                    $this->module->displayName,
+                    null,
+                    $extra_vars,
+                    (int)$currency->id,
+                    false,
+                    $customer->secure_key
+                );
                 $new_order = new Order((int)$this->module->currentOrder);
                 if (Validate::isLoadedObject($new_order)) {
                     $payment = $new_order->getOrderPaymentCollection();
@@ -115,24 +126,47 @@ class AdyenValidate3dModuleFrontController extends \Adyen\PrestaShop\controllers
                         $payment[0]->save();
                     }
                 }
-                Tools::redirect($this->context->link->getPageLink('order-confirmation', $this->ssl, null,
-                    'id_cart=' . $cart->id . '&id_module=' . $this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key));
+                Tools::redirect($this->context->link->getPageLink(
+                    'order-confirmation',
+                    $this->ssl,
+                    null,
+                    sprintf(
+                        "id_cart=%s&id_module=%s&id_order=%s&key=%s",
+                        $cart->id,
+                        $this->module->id,
+                        $this->module->currentOrder,
+                        $customer->secure_key
+                    )
+                ));
                 break;
             case 'Refused':
                 // create new cart from the current cart
                 $this->helperData->cloneCurrentCart($this->context, $cart);
 
                 $this->logger->error("The payment was refused, id:  " . $cart->id);
-                $this->setTemplate($this->helperData->getTemplateFromModulePath('views/templates/front/error.tpl'));
+                $this->setTemplate(
+                    $this->helperData->getTemplateFromModulePath('views/templates/front/error.tpl')
+                );
                 break;
             default:
                 // create new cart from the current cart
                 $this->helperData->cloneCurrentCart($this->context, $cart);
                 //6_PS_OS_CANCELED_ : order canceled
-                $this->module->validateOrder($cart->id, 6, $total, $this->module->displayName, null, $extra_vars,
-                    (int)$currency->id, false, $customer->secure_key);
+                $this->module->validateOrder(
+                    $cart->id,
+                    6,
+                    $total,
+                    $this->module->displayName,
+                    null,
+                    $extra_vars,
+                    (int)$currency->id,
+                    false,
+                    $customer->secure_key
+                );
                 $this->logger->error("The payment was cancelled, id:  " . $cart->id);
-                $this->setTemplate($this->helperData->getTemplateFromModulePath('views/templates/front/error.tpl'));
+                $this->setTemplate(
+                    $this->helperData->getTemplateFromModulePath('views/templates/front/error.tpl')
+                );
                 break;
         }
     }
