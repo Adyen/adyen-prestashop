bb50c48c62a1a13d2e9ebfa82dbc95a741cc261c
marcoss <marcos.silvagarcia@adyen.com>
2020-02-14 13:26:30 +0100

    Fix imports

--- service/adapter/classes/State.php
+++ service/adapter/classes/State.php
@@ -23,12 +23,16 @@
 
 namespace Adyen\PrestaShop\service\adapter\classes;
 
+use Cache;
+use Db;
+
 class State
 {
     /**
      * Retrieves Iso code for a state by id
      *
      * @param $id_state
+     *
      * @return bool
      */
     public function getIsoById($id_state)
@@ -38,17 +42,17 @@ class State
         }
 
         $cache_id = 'State::getIsoById_' . (int)$id_state;
-        if (!\Cache::isStored($cache_id)) {
+        if (!Cache::isStored($cache_id)) {
             $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
 				SELECT `iso_code`
 				FROM `' . _DB_PREFIX_ . 'state`
 				WHERE `id_state` = ' . (int)$id_state
             );
 
-            \Cache::store($cache_id, $result);
+            Cache::store($cache_id, $result);
             return $result;
         }
 
-        return \Cache::retrieve($cache_id);
+        return Cache::retrieve($cache_id);
     }
 }
