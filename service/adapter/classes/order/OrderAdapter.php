691a5447038e19fb110e5069b667b0aef52a20fb
marcoss <marcos.silvagarcia@adyen.com>
2020-02-14 14:46:41 +0100

    Add @license to copyright block

--- service/adapter/classes/order/OrderAdapter.php
+++ service/adapter/classes/order/OrderAdapter.php
@@ -18,6 +18,7 @@
  *
  * @author Adyen BV <support@adyen.com>
  * @copyright (c) 2020 Adyen B.V.
+ * @license https://opensource.org/licenses/MIT MIT license
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  */
