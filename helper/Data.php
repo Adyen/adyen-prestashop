2835b96be607a6b8f2c439d7a69cef8ab77530fe
attilak <attila.kiss@adyen.com>
2020-03-09 14:10:16 +0100

    Create Cart service to cloneCurrentCart()
    Remove the function from the helper/Data.php

--- helper/Data.php
+++ helper/Data.php
@@ -360,63 +360,6 @@ class Data
     }
 
     /**
-     * @param $context
-     * @param $cart
-     *
-     * @return void
-     * @throws PrestaShopDatabaseException
-     * @throws PrestaShopException
-     */
-    public function cloneCurrentCart($context, Cart $cart)
-    {
-        // To save the secure key of current cart id and reassign the same to new cart
-        $old_cart_secure_key = $cart->secure_key;
-        // To save the customer id of current cart id and reassign the same to new cart
-        $old_cart_customer_id = (int)$cart->id_customer;
-
-        // To fetch the current cart products
-        $cart_products = $cart->getProducts();
-        // Creating new cart object
-        $context->cart = new Cart();
-        $context->cart->id_lang = $context->language->id;
-
-        $context->cart->id_currency = $context->currency->id;
-        $context->cart->secure_key = $old_cart_secure_key;
-        // to add new cart
-        $context->cart->add();
-        // to update the new cart
-        foreach ($cart_products as $product) {
-            $context->cart->updateQty(
-                (int)$product['quantity'],
-                (int)$product['id_product'],
-                (int)$product['id_product_attribute']
-            );
-        }
-        if ($context->cookie->id_guest) {
-            $guest = new Guest($context->cookie->id_guest);
-            $context->cart->mobile_theme = $guest->mobile_theme;
-        }
-        // to map the new cart with the customer
-        $context->cart->id_customer = $old_cart_customer_id;
-        // to save the new cart
-        $context->cart->save();
-        if ($context->cart->id) {
-            $context->cookie->id_cart = (int)$context->cart->id;
-            $context->cookie->write();
-        }
-    }
-
-    /**
-     * Start the session if does not exists yet
-     */
-    public function startSession()
-    {
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-    }
-
-    /**
      * Get locale for 1.6/1.7
      * @return mixed
      * @deprecated use Adyen\PrestaShop\service\adapter\classes\Language
