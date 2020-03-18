674ee3d854cf8d7c53368a64b09b2e64a7c492ec
attilak <attila.kiss@adyen.com>
2020-03-18 14:21:37 +0100

    Add Configuration::get adapter to support default in both 1.6 and 1.7

--- service/adapter/classes/Configuration.php
+++ service/adapter/classes/Configuration.php
@@ -25,8 +25,8 @@
 namespace Adyen\PrestaShop\service\adapter\classes;
 
 use Adyen\Environment;
+use Adyen\PrestaShop\application\VersionChecker;
 use Adyen\PrestaShop\service\Logger;
-use Tools;
 
 class Configuration
 {
@@ -71,32 +71,41 @@ class Configuration
     private $logger;
 
     /**
+     * @var VersionChecker
+     */
+    private $versionChecker;
+
+    /**
      * Configuration constructor.
      *
      * @param Logger $logger
+     * @param VersionChecker $versionChecker
      */
-    public function __construct(Logger $logger)
+    public function __construct(
+        Logger $logger,
+        VersionChecker $versionChecker
+    )
     {
         $this->logger = $logger;
-        $this->httpHost = Tools::getHttpHost(true, true);
+        $this->httpHost = \Tools::getHttpHost(true, true);
         $adyenModeConfiguration = \Configuration::get('ADYEN_MODE');
         $this->adyenMode = !empty($adyenModeConfiguration) ? $adyenModeConfiguration : Environment::TEST;
         $this->sslEncryptionKey = _COOKIE_KEY_;
-        $this->encryptedApiKey = $this->getEncryptedAPIKey($this->adyenMode);
+        $this->encryptedApiKey = $this->getEncryptedAPIKey();
         $this->liveEndpointPrefix = \Configuration::get('ADYEN_LIVE_ENDPOINT_URL_PREFIX');
         $this->moduleVersion = '2.0.0';
         $this->moduleName = 'adyen-prestashop';
+        $this->versionChecker = $versionChecker;
     }
 
     /**
      * Retrieves the encrypted API key based on the mode set in the admin configurations
      *
-     * @param string $adyenRunningMode
      * @return string
      */
-    private function getEncryptedAPIKey($adyenRunningMode)
+    private function getEncryptedAPIKey()
     {
-        if ($this->isTestMode($adyenRunningMode)) {
+        if ($this->isTestMode()) {
             $encryptedApiKey = \Configuration::get('ADYEN_APIKEY_TEST');
         } else {
             $encryptedApiKey = \Configuration::get('ADYEN_APIKEY_LIVE');
@@ -115,6 +124,30 @@ class Configuration
             return true;
         } else {
             return false;
+        }
+    }
+
+    /**
+     * Supports default for both PrestaShop 1.6 and 1.7
+     *
+     * @param $key
+     * @param null $id_lang
+     * @param null $id_shop_group
+     * @param null $id_shop
+     * @param bool $default
+     * @return bool
+     */
+    public function get($key, $id_lang = null, $id_shop_group = null, $id_shop = null, $default = false)
+    {
+        if ($this->versionChecker->isPrestaShop16()) {
+            $configurationValue = \Configuration::get($key, $id_lang, $id_shop_group, $id_shop);
+            if (false === $configurationValue && false !== $default) {
+                return $default;
+            }
+
+            return $configurationValue;
+        } else {
+            return \Configuration::get($key, $id_lang, $id_shop_group, $id_shop, $default);
         }
     }
 }
