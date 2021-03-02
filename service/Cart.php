1b28ce11cb86ba4534961f2e152443a2e89ef5f5
Jeantwan Teuma <Morerice@users.noreply.github.com>
2021-03-02 16:27:10 +0100

    [PW-3071] - How to implement a nicer error page than the current in 1.7 (#165)
    * PW-3071 - Copy the checkout_session_data and when creating a duplicate cart and redirect to the order page
    
    * PW-3071 - Create function to show redirectErrorMessage if it exists
    
    * PW-3071 - Only redirect to order page and add query string if on 1.7. Also, refactor js call
    
    * PW-3071 - Copy id_guest in the clone cart function
    
    * PW-3071 - Create and use newCartId variable and add comments on db operations

--- service/Cart.php
+++ service/Cart.php
@@ -49,6 +49,7 @@ class Cart
         $context->cart->secure_key = $old_cart_secure_key;
         // to add new cart
         $context->cart->add();
+        $newCartId  = $context->cart->id;
         // to update the new cart
         foreach ($cart_products as $product) {
             $context->cart->updateQty(
@@ -61,8 +62,21 @@ class Cart
             $guest = new \Guest($context->cookie->id_guest);
             $context->cart->mobile_theme = $guest->mobile_theme;
         }
+
+        // Get the checkout_session_data field of the previous cart
+        $checkoutSessionData = \Db::getInstance()->getValue(
+            'SELECT checkout_session_data FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . (int) $cart->id
+        );
+
+        // Update the checkout_session_data field of the new cart
+        \Db::getInstance()->execute(
+            'UPDATE ' . _DB_PREFIX_ . 'cart SET checkout_session_data = "' . pSQL($checkoutSessionData) . '"
+        WHERE id_cart = ' . (int) $newCartId
+        );
+
         // to map the new cart with the customer
         $context->cart->id_customer = $old_cart_customer_id;
+        $context->cart->id_guest = $cart->id_guest;
         // to save the new cart
         $context->cart->save();
         if ($context->cart->id) {
