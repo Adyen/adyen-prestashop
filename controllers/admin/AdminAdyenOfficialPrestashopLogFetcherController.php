7f290015cf860945a861214a258a07ab918b384e
jeant <jean.teuma@adyen.com>
2021-04-06 09:59:52 +0200

    PW-4200 - Create functionality to zip and download logs

--- controllers/admin/AdminAdyenOfficialPrestashopLogFetcherController.php
+++ controllers/admin/AdminAdyenOfficialPrestashopLogFetcherController.php
@@ -27,10 +27,13 @@
 // phpcs:disable PSR1.Files.SideEffects, PSR1.Classes.ClassDeclaration
 
 use PrestaShop\PrestaShop\Adapter\CoreException;
+use PrestaShopBundle\Utils\ZipManager;
 
 
 class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminController
 {
+    /** @var ZipManager $zipManager */
+    private $zipManager;
 
     /**
      * AdminAdyenPrestashopCronController constructor.
@@ -39,17 +42,48 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
      */
     public function __construct()
     {
-        // Set variables
-        $this->display = 'view';
+        $this->zipManager = \PrestaShop\PrestaShop\Adapter\ServiceLocator::get('PrestaShopBundle\Utils\ZipManager');
 
+        // Required in order to automatically call the renderView function
+        $this->display = 'view';
         $this->bootstrap = true;
+        $this->toolbar_title[] = 'Adyen Logs';
         parent::__construct();
+
+        $this->zipAndDownload();
     }
 
     public function renderView()
     {
-        $tpl = $this->context->smarty->createTemplate(_PS_MODULE_DIR_ . $this->module->name.'/views/templates/admin/log-fetcher.tpl');
+        $tpl = $this->context->smarty->createTemplate(
+            _PS_MODULE_DIR_ . $this->module->name . '/views/templates/admin/log-fetcher.tpl'
+        );
 
         return $tpl->fetch();
+    }
+
+    /**
+     * Zip log files and download
+     */
+    public function zipAndDownload()
+    {
+        $dir = _PS_ROOT_DIR_ . '/var/logs/adyen';
+        $zip_file = Configuration::get('PS_SHOP_NAME') . '_' . date('Y-m-d') . '_' . 'Adyen_Logs.zip';
+
+        // Get real path for our folder
+        $rootPath = realpath($dir);
+
+        $this->zipManager->createArchive($zip_file, $rootPath);
+
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($zip_file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: no-cache');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($zip_file));
+        readfile($zip_file);
     }
 }
