d6a1414f7b37ae16d62fffc4532dc75348da89b0
jeant <jean.teuma@adyen.com>
2021-04-06 14:14:55 +0200

    PW-4200 - Create functions to add a text file which also contains all the config values and the applicationInfo values

--- controllers/admin/AdminAdyenOfficialPrestashopLogFetcherController.php
+++ controllers/admin/AdminAdyenOfficialPrestashopLogFetcherController.php
@@ -26,6 +26,9 @@
 // Controllers, which breaks a PSR1 element.
 // phpcs:disable PSR1.Files.SideEffects, PSR1.Classes.ClassDeclaration
 
+use Adyen\PrestaShop\exception\GenericLoggedException;
+use Adyen\PrestaShop\exception\MissingDataException;
+use Adyen\PrestaShop\service\adapter\classes\ServiceLocator;
 use PrestaShop\PrestaShop\Adapter\CoreException;
 use PrestaShopBundle\Utils\ZipManager;
 
@@ -35,6 +38,14 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
     /** @var ZipManager $zipManager */
     private $zipManager;
 
+    /** @var \Adyen\PrestaShop\service\adapter\classes\Configuration $configuration */
+    private $configuration;
+
+    /**
+     * @var Adyen\PrestaShop\infra\Crypto
+     */
+    private $crypto;
+
     /**
      * AdminAdyenPrestashopCronController constructor.
      *
@@ -42,7 +53,9 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
      */
     public function __construct()
     {
-        $this->zipManager = \PrestaShop\PrestaShop\Adapter\ServiceLocator::get('PrestaShopBundle\Utils\ZipManager');
+        $this->zipManager = ServiceLocator::get('PrestaShopBundle\Utils\ZipManager');
+        $this->configuration = ServiceLocator::get('Adyen\PrestaShop\service\adapter\classes\Configuration');
+        $this->crypto = ServiceLocator::get('Adyen\PrestaShop\infra\Crypto');
 
         // Required in order to automatically call the renderView function
         $this->display = 'view';
@@ -50,6 +63,7 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
         $this->toolbar_title[] = 'Adyen Logs';
         parent::__construct();
 
+        $this->createCurrentApplicationInfoFile();
         $this->zipAndDownload();
     }
 
@@ -63,9 +77,9 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
     }
 
     /**
-     * Zip log files and download
+     * Zip all files in the adyen log directory and download
      */
-    public function zipAndDownload()
+    private function zipAndDownload()
     {
         $dir = _PS_ROOT_DIR_ . '/var/logs/adyen';
         $zip_file = Configuration::get('PS_SHOP_NAME') . '_' . date('Y-m-d') . '_' . 'Adyen_Logs.zip';
@@ -85,5 +99,84 @@ class AdminAdyenOfficialPrestashopLogFetcherController extends ModuleAdminContro
         header('Pragma: public');
         header('Content-Length: ' . filesize($zip_file));
         readfile($zip_file);
+    }
+
+    /**
+     * Create a text file with the current application info
+     */
+    private function createCurrentApplicationInfoFile()
+    {
+        $adyenPaymentSource = sprintf(
+            'Adyen module version: %s',
+            $this->configuration->moduleVersion
+        );
+
+        $prestashopVersion = sprintf("\nPrestashop version: %s", _PS_VERSION_);
+
+        if (!empty($this->configuration->integratorName)) {
+            $prestashopVersion .= sprintf(' with integrator: %s', $this->configuration->integratorName);
+        }
+
+        $environment = sprintf(
+            "\nEnvironment: %s, with live endpoint prefix: %s",
+            $this->configuration->adyenMode,
+            $this->configuration->liveEndpointPrefix
+        );
+
+        $content = $adyenPaymentSource . $prestashopVersion . $environment . $this->getConfigurationValues();
+
+        $filePath = fopen(_PS_ROOT_DIR_ . '/var/logs/adyen' . "/applicationInfo","wb");
+        fwrite($filePath,$content);
+        fclose($filePath);
+    }
+
+    /**
+     * Get all remaining config values
+     *
+     * @return string
+     * @throws GenericLoggedException
+     * @throws MissingDataException
+     */
+    private function getConfigurationValues()
+    {
+        $configValues = "\n\nConfiguration values: \n";
+        $configs['autoCronJobRunner'] = sprintf("\nAuto cronjob runner: %s", Configuration::get('ADYEN_AUTO_CRON_JOB_RUNNER'));
+        $configs['storedPaymentMethods'] = sprintf("\nStored payment methods: %s", Configuration::get('ADYEN_ENABLE_STORED_PAYMENT_METHODS'));
+        $configs['displayCollapse'] = sprintf("\nPayment display collapse: %s", Configuration::get('ADYEN_PAYMENT_DISPLAY_COLLAPSE'));
+        $configs['merchantAccount'] = sprintf("\nMerchant account: %s", Configuration::get('ADYEN_MERCHANT_ACCOUNT'));
+        $configs['mode'] = sprintf("\nMode: %s", Configuration::get('ADYEN_MODE'));
+        $configs['notificationUser'] = sprintf("\nNotification user: %s", Configuration::get('ADYEN_NOTI_USERNAME'));
+        $configs['clientKeyTest'] = sprintf("\nClient key test: %s", Configuration::get('ADYEN_CLIENTKEY_TEST'));
+        $configs['clientKeyLive'] = sprintf("\nClient key live: %s", Configuration::get('ADYEN_CLIENTKEY_LIVE'));
+        $configs['appleName'] = sprintf("\nApple pay merchant name: %s", Configuration::get('ADYEN_APPLE_PAY_MERCHANT_NAME'));
+        $configs['appleIdentifier'] = sprintf("\nApple pay merchant identifier: %s", Configuration::get('ADYEN_APPLE_PAY_MERCHANT_IDENTIFIER'));
+        $configs['googleGatewayMerchant'] = sprintf("\nGoogle pay merchant gateway id: %s", Configuration::get('ADYEN_APPLE_PAY_MERCHANT_NAME'));
+        $configs['googleIdentifier'] = sprintf("\nGoogle pay merchant identifier: %s", Configuration::get('ADYEN_GOOGLE_PAY_MERCHANT_IDENTIFIER'));
+
+        $configs['notificationPass'] = sprintf(
+            "\nNotification password last 4: %s",
+            substr($this->crypto->decrypt(Configuration::get('ADYEN_NOTI_PASSWORD')), -4)
+        );
+
+        $configs['notificationHmac'] = sprintf(
+            "\nNotification HMAC last 4: %s",
+            substr($this->crypto->decrypt(Configuration::get('ADYEN_NOTI_HMAC')), -4)
+        );
+
+        $configs['apiKeyTest'] = sprintf(
+            "\nApi key test last 4: %s",
+            substr($this->crypto->decrypt(Configuration::get('ADYEN_APIKEY_TEST')), -4)
+        );
+
+        $configs['apiKeyLive'] = sprintf(
+            "\nApi key live last 4: %s",
+            substr($this->crypto->decrypt(Configuration::get('ADYEN_APIKEY_LIVE')), -4)
+        );
+
+        foreach ($configs as $config) {
+            $configValues .= $config;
+        }
+
+        return $configValues;
     }
 }
