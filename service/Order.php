0549bbeb0a10e6c82ba2a0d5c5e3b06d7b656729
jeant <jean.teuma@adyen.com>
2021-04-29 15:36:44 +0200

    PW-4368 - Create functionality to get the order_conf template variables just in case the merchant would like to send that email instead of the payment successful

--- service/Order.php
+++ service/Order.php
@@ -25,6 +25,16 @@
 namespace Adyen\PrestaShop\service;
 
 use Adyen\PrestaShop\service\adapter\classes\ServiceLocator;
+use OrderCore;
+use PrestaShop\PrestaShop\Adapter\Entity\Address;
+use PrestaShop\PrestaShop\Adapter\Entity\AddressFormat;
+use PrestaShop\PrestaShop\Adapter\Entity\Carrier;
+use PrestaShop\PrestaShop\Adapter\Entity\Configuration;
+use PrestaShop\PrestaShop\Adapter\Entity\Currency;
+use PrestaShop\PrestaShop\Adapter\Entity\Product;
+use PrestaShop\PrestaShop\Adapter\Entity\Tools;
+use PrestaShop\PrestaShop\Core\Localization\Exception\LocalizationException;
+use PrestaShopBundle\Translation\Translator;
 
 class Order
 {
@@ -90,6 +100,7 @@ class Order
      */
     public function updateOrderState($order, $orderStateId)
     {
+        $templateVars = array();
         // check if the new order state is the same as the current state
         $currentOrderStateId = (int)$order->getCurrentState();
 
@@ -110,7 +121,14 @@ class Order
             $useExistingPayment
         );
 
-        if (!$orderHistory->addWithemail()) {
+        // If order is going from Waiting for Payment to Successful, get the orderConf email template vars in case
+        // the merchant would like to send that email instead of the payment successful one
+        if ($currentOrderStateId === (int)Configuration::get('ADYEN_OS_WAITING_FOR_PAYMENT') &&
+            $orderStateId === Configuration::get('PS_OS_PAYMENT')) {
+            $templateVars = $this->getOrderConfEmailTemplateVariables($order);
+        }
+
+        if (!$orderHistory->addWithemail(true, $templateVars)) {
             $this->logger->addError(
                 'Email was not sent upon order state update',
                 array("order id" => $order->id, "new state id" => $orderStateId)
@@ -118,5 +136,61 @@ class Order
         }
 
         return $order;
+    }
+
+    /**
+     * This function gets all the data shown in the default order_conf email, similar to what happens in
+     * PaymentModule::validateOrder
+     *
+     * TODO: Check if this should be completely omitted for 1.6
+     *
+     * @param OrderCore $order
+     * @return array
+     * @throws \PrestaShopException
+     * @throws LocalizationException
+     */
+    private function getOrderConfEmailTemplateVariables(OrderCore $order)
+    {
+        $currency = new Currency((int) $order->id_currency, null);
+        $virtualOrder = $order->isVirtual();
+        $carrier = $order->id_carrier ? new Carrier($order->id_carrier) : false;
+        $invoice = new Address((int) $order->id_address_invoice);
+        $delivery = new Address((int) $order->id_address_delivery);
+
+        $data = [
+            '{date}' => Tools::displayDate(date('Y-m-d H:i:s'), null, 1),
+            '{payment}' => Tools::substr($order->payment, 0, 255),
+            '{total_products}' => Tools::displayPrice(Product::getTaxCalculationMethod() == PS_TAX_EXC ?
+                $order->total_products : $order->total_products_wt, $currency, false),
+            '{total_discounts}' => Tools::displayPrice($order->total_discounts, $currency, false),
+            '{total_wrapping}' => Tools::displayPrice($order->total_wrapping, $currency, false),
+            '{total_shipping}' => Tools::displayPrice($order->total_shipping, $currency, false),
+            '{total_tax_paid}' => Tools::displayPrice(($order->total_products_wt - $order->total_products) +
+                ($order->total_shipping_tax_incl - $order->total_shipping_tax_excl), $currency, false),
+            '{carrier}' => ($virtualOrder || !isset($carrier->name)) ? 'No carrier' : $carrier->name,
+            '{delivery_block_html}' => AddressFormat::generateAddress(
+                $delivery,
+                array('avoid' => array()),
+                '<br />',
+                ' ',
+                array(
+                    'firstname' => '<span style="font-weight:bold;">%s</span>',
+                    'lastname' => '<span style="font-weight:bold;">%s</span>',
+                )
+            ),
+            '{invoice_block_html}' => AddressFormat::generateAddress(
+                $invoice,
+                array('avoid' => array()),
+                '<br />',
+                ' ',
+                array(
+                    'firstname' => '<span style="font-weight:bold;">%s</span>',
+                    'lastname' => '<span style="font-weight:bold;">%s</span>',
+                )
+            ),
+
+        ];
+
+        return $data;
     }
 }
