2cbbfa08cbb303fc62ab3ed65207d6ed3515958b
jeant <jean.teuma@adyen.com>
2021-05-05 14:22:55 +0200

    PW-4368 - Move contents of 3.5.0 upgrade script to 3.6.0, since 3.5.0 has already been released

--- upgrade/upgrade-3.6.0.php
+++ upgrade/upgrade-3.6.0.php
@@ -39,5 +39,75 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_3_6_0(AdyenOfficial $module)
 {
-    return $module->copyEmailTemplates();
+    return $module->copyEmailTemplates() && install_new_tabs($module);
+}
+
+/**
+ * @param AdyenOfficial $module
+ *
+ * @return bool
+ */
+function install_new_tabs(AdyenOfficial $module)
+{
+    if (version_compare(_PS_VERSION_, '1.7', '<')) {
+        $parentTab = (int)Tab::getIdFromClassName('AdminParentModules');
+        $namePrefix = 'Adyen ';
+        $adyenTabResult = true;
+    } else {
+        $parentTab = (int)Tab::getIdFromClassName('AdminAdyenOfficialPrestashop');
+        if (!$parentTab) {
+            // Parent adyen tab
+            $adyenTab = new Tab();
+            $adyenTab->id_parent = (int)Tab::getIdFromClassName('AdminParentModulesSf');
+            $adyenTab->active = 1;
+            $adyenTab->name = array();
+            foreach (Language::getLanguages() as $lang) {
+                $adyenTab->name[$lang['id_lang']] = 'Adyen Module';
+            }
+            $adyenTab->class_name = 'AdminAdyenOfficialPrestashop';
+            $adyenTab->module = $module->name;
+
+            $adyenTabResult = $adyenTab->add();
+            $parentTab = (int)Tab::getIdFromClassName('AdminAdyenOfficialPrestashop');
+        } else {
+            $adyenTabResult = true;
+        }
+        $namePrefix = '';
+    }
+
+    $logFetcherTabId = (int)Tab::getIdFromClassName('AdminAdyenOfficialPrestashopLogFetcher');
+    if (!$logFetcherTabId) {
+        // Log tab
+        $logTab = new Tab();
+        $logTab->id_parent = $parentTab;
+        $logTab->active = 1;
+        $logTab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $logTab->name[$lang['id_lang']] = $namePrefix . 'Logs';
+        }
+        $logTab->class_name = 'AdminAdyenOfficialPrestashopLogFetcher';
+        $logTab->module = $module->name;
+        $logTabResult = $logTab->add();
+    } else {
+        $logTabResult = true;
+    }
+
+    $validatorTabId = (int)Tab::getIdFromClassName('AdminAdyenOfficialPrestashopValidator');
+    if (!$validatorTabId) {
+        // Validator tab
+        $validatorTab = new Tab();
+        $validatorTab->id_parent = $parentTab;
+        $validatorTab->active = 1;
+        $validatorTab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $validatorTab->name[$lang['id_lang']] = $namePrefix . 'Validator';
+        }
+        $validatorTab->class_name = 'AdminAdyenOfficialPrestashopValidator';
+        $validatorTab->module = $module->name;
+        $validatorTabResult = $validatorTab->add();
+    } else {
+        $validatorTabResult = true;
+    }
+
+    return $logTabResult && $adyenTabResult && $validatorTabResult;
 }
