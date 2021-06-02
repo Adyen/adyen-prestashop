89fe7eeca2d71b793b0555b6dee3615e2ac8fbd7
jeant <jean.teuma@adyen.com>
2021-06-02 13:23:35 +0200

    PW-4781 - Use the correct name of the hook on 1.7 for validator and call cloneCurrentCart correctly

--- adyenofficial.php
+++ adyenofficial.php
@@ -235,7 +235,7 @@ class AdyenOfficial extends PaymentModule
                 'displayPaymentTop',
                 'actionFrontControllerSetMedia',
                 'paymentOptions',
-                'displayPaymentReturn',
+                'paymentReturn',
                 'actionOrderSlipAdd',
                 'actionEmailSendBefore'
             )
