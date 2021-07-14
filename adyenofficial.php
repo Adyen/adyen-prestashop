e18e4924fea9cf5181c1018738adcd9e760e35ec
attilak <attila.kiss@adyen.com>
2021-07-14 09:56:05 +0200

    Version bump 3.7.1

--- adyenofficial.php
+++ adyenofficial.php
@@ -127,7 +127,7 @@ class AdyenOfficial extends PaymentModule
     public function __construct()
     {
         $this->name = 'adyenofficial';
-        $this->version = '3.7.0';
+        $this->version = '3.7.1';
         $this->tab = 'payments_gateways';
         $this->author = 'Adyen';
         $this->bootstrap = true;
