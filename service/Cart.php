67c020bdd81dbb4a06000d1a7b96bfff838467a3
Cédric Dué <cedric.due@ysely.io>
2022-11-23 16:02:29 +0100

    fix: Cart cloned without product customization after a canceled/refused payment

--- service/Cart.php
+++ service/Cart.php
@@ -24,8 +24,21 @@
 
 namespace Adyen\PrestaShop\service;
 
+use Adyen\PrestaShop\service\adapter\classes\ServiceLocator;
+use CartRuleCore as CartRule;
+
 class Cart
 {
+    /**
+     * @var Adyen\PrestaShop\service\Logger
+     */
+    private $logger;
+ 
+    public function __construct()
+    {
+        $this->logger = ServiceLocator::get('Adyen\PrestaShop\service\Logger');
+    }
+
     /**
      * Clones cart and updates the context
      *
@@ -33,66 +46,37 @@ class Cart
      * @param \Cart $cart
      * @param bool $isPrestashop16
      */
-    public function cloneCurrentCart(\Context $context, \Cart $cart, $isPrestashop16)
+    public function cloneCurrentCart(\Context $context, \Cart $cart, bool $isPrestashop16)
     {
-        // To save the secure key of current cart id and reassign the same to new cart
-        $old_cart_secure_key = $cart->secure_key;
-        // To save the customer id of current cart id and reassign the same to new cart
-        $old_cart_customer_id = (int)$cart->id_customer;
-        $old_delivery_address_id = $cart->id_address_delivery;
-        $old_invoice_address_id = $cart->id_address_invoice;
+        $duplication = $cart->duplicate();
 
-        // To fetch the current cart products
-        $cart_products = $cart->getProducts();
-        // Creating new cart object
-        $context->cart = new \Cart();
-        $context->cart->id_lang = $context->language->id;
-        $context->cart->id_address_delivery = $old_delivery_address_id;
-        $context->cart->id_address_invoice = $old_invoice_address_id;
-
-        $context->cart->id_currency = $context->currency->id;
-        $context->cart->secure_key = $old_cart_secure_key;
-        // to add new cart
-        $context->cart->add();
-        $newCartId  = $context->cart->id;
-        // to update the new cart
-        foreach ($cart_products as $product) {
-            $context->cart->updateQty(
-                (int)$product['quantity'],
-                (int)$product['id_product'],
-                (int)$product['id_product_attribute']
-            );
-        }
-        if ($context->cookie->id_guest) {
-            $guest = new \Guest($context->cookie->id_guest);
-            $context->cart->mobile_theme = $guest->mobile_theme;
-        }
-
-        // Field does not exist in prestashop 16
-        // Else set the cart qties manually to ensure consistency with layout header
-        if (!$isPrestashop16) {
-            // Get the checkout_session_data field of the previous cart
-            $checkoutSessionData = \Db::getInstance()->getValue(
-                'SELECT checkout_session_data FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . (int)$cart->id
-            );
-
-            // Update the checkout_session_data field of the new cart
-            \Db::getInstance()->execute(
-                'UPDATE ' . _DB_PREFIX_ . 'cart SET checkout_session_data = "' . pSQL($checkoutSessionData) . '"
-        WHERE id_cart = ' . (int)$newCartId
-            );
+        if (!$duplication
+            || !is_object($duplication['cart'])
+            || !$duplication['cart']->id
+            || !$duplication['success']) {
+            $this->logger->error('Adyen module was unable to duplicate cart with id: ' . $cart->id);
         } else {
-            $context->smarty->assign('cart_qties', $context->cart->nbProducts());
-        }
+            $context->cookie->id_cart = $duplication['cart']->id;
+            $context->cart = $duplication['cart'];
+            CartRule::autoAddToCart($context);
 
-        // to map the new cart with the customer
-        $context->cart->id_customer = $old_cart_customer_id;
-        $context->cart->id_guest = $cart->id_guest;
+            // Field does not exist in prestashop 16
+            // Else set the cart qties manually to ensure consistency with layout header
+            if (!$isPrestashop16) {
+                // Get the checkout_session_data field of the previous cart
+                $checkoutSessionData = \Db::getInstance()->getValue(
+                    'SELECT checkout_session_data FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . (int)$cart->id
+                );
 
-        // to save the new cart
-        $context->cart->save();
-        if ($context->cart->id) {
-            $context->cookie->id_cart = (int)$context->cart->id;
+                // Update the checkout_session_data field of the new cart
+                \Db::getInstance()->execute(
+                    'UPDATE ' . _DB_PREFIX_ . 'cart SET checkout_session_data = "' . pSQL($checkoutSessionData) . '"
+                    WHERE id_cart = ' . (int)$context->cart->id
+                );
+            } else {
+                $context->smarty->assign('cart_qties', $context->cart->nbProducts());
+            }
+
             $context->cookie->write();
         }
     }
