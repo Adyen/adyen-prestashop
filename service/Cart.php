f25c41c5ddc7bf69c7c92285c0869f4b2e70d7fa
Goran Stamenkovski <goran.stamenkovski@logeecom.com>
2022-12-07 13:58:11 +0100

    Refactor contribution code from PR##254
    ISSUE: CS-3910

--- service/Cart.php
+++ service/Cart.php
@@ -24,21 +24,8 @@
 
 namespace Adyen\PrestaShop\service;
 
-use Adyen\PrestaShop\service\adapter\classes\ServiceLocator;
-use CartRuleCore as CartRule;
-
 class Cart
 {
-    /**
-     * @var Adyen\PrestaShop\service\Logger
-     */
-    private $logger;
- 
-    public function __construct()
-    {
-        $this->logger = ServiceLocator::get('Adyen\PrestaShop\service\Logger');
-    }
-
     /**
      * Clones cart and updates the context
      *
@@ -46,38 +33,33 @@ class Cart
      * @param \Cart $cart
      * @param bool $isPrestashop16
      */
-    public function cloneCurrentCart(\Context $context, \Cart $cart, bool $isPrestashop16)
+    public function cloneCurrentCart(\Context $context, \Cart $cart, $isPrestashop16)
     {
         $duplication = $cart->duplicate();
-
-        if (!$duplication
-            || !is_object($duplication['cart'])
-            || !$duplication['cart']->id
-            || !$duplication['success']) {
-            $this->logger->error('Adyen module was unable to duplicate cart with id: ' . $cart->id);
-        } else {
-            $context->cookie->id_cart = $duplication['cart']->id;
-            $context->cart = $duplication['cart'];
-            CartRule::autoAddToCart($context);
-
-            // Field does not exist in prestashop 16
-            // Else set the cart qties manually to ensure consistency with layout header
-            if (!$isPrestashop16) {
-                // Get the checkout_session_data field of the previous cart
-                $checkoutSessionData = \Db::getInstance()->getValue(
-                    'SELECT checkout_session_data FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . (int)$cart->id
-                );
-
-                // Update the checkout_session_data field of the new cart
-                \Db::getInstance()->execute(
-                    'UPDATE ' . _DB_PREFIX_ . 'cart SET checkout_session_data = "' . pSQL($checkoutSessionData) . '"
-                    WHERE id_cart = ' . (int)$context->cart->id
-                );
-            } else {
-                $context->smarty->assign('cart_qties', $context->cart->nbProducts());
-            }
-
-            $context->cookie->write();
+        if (!$duplication || !$duplication['success'] || !\Validate::isLoadedObject($duplication['cart'])) {
+            return;
         }
+
+        $context->cookie->id_cart = $duplication['cart']->id;
+        $context->cart = $duplication['cart'];
+        $context->smarty->assign('cart_qties', $context->cart->nbProducts());
+        \CartRule::autoAddToCart($context);
+
+        // Field does not exist in prestashop 16
+        // Else set the cart qties manually to ensure consistency with layout header
+        if (!$isPrestashop16) {
+            // Get the checkout_session_data field of the previous cart
+            $checkoutSessionData = \Db::getInstance()->getValue(
+                'SELECT checkout_session_data FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . (int)$cart->id
+            );
+
+            // Update the checkout_session_data field of the new cart
+            \Db::getInstance()->execute(
+                'UPDATE ' . _DB_PREFIX_ . 'cart SET checkout_session_data = "' . pSQL($checkoutSessionData) . '"
+                WHERE id_cart = ' . (int)$context->cart->id
+            );
+        }
+
+        $context->cookie->write();
     }
 }
