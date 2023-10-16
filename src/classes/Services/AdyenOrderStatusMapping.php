eec3928ad680133968cab65c259c813669d76a09
Marija <marija.ivic@logeecom.com>
2023-10-16 14:35:32 +0200

    Add fallback order mapping status in case native status is deleted
    ISSUE CS-4743

--- src/classes/Services/AdyenOrderStatusMapping.php
+++ src/classes/Services/AdyenOrderStatusMapping.php
@@ -86,17 +86,25 @@ class AdyenOrderStatusMapping
     private static function mapPrestaStatuses(): array
     {
         return [
-            PaymentStates::STATE_IN_PROGRESS => static::getPrestaShopOrderStatusId(self::PRESTA_PROCESSING_IN_PROGRESS),
-            PaymentStates::STATE_PENDING => static::getPrestaShopOrderStatusId(self::PRESTA_PENDING),
-            PaymentStates::STATE_PAID => static::getPrestaShopOrderStatusId(self::PRESTA_PAYMENT_ACCEPTED),
-            PaymentStates::STATE_FAILED => static::getPrestaShopOrderStatusId(self::PRESTA_PAYMENT_ERROR),
-            PaymentStates::STATE_REFUNDED => static::getPrestaShopOrderStatusId(self::PRESTA_REFUNDED),
+            PaymentStates::STATE_IN_PROGRESS => static::getPrestaShopOrderStatusId(self::PRESTA_PROCESSING_IN_PROGRESS)
+                ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_PENDING => static::getPrestaShopOrderStatusId(self::PRESTA_PENDING)
+                ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_PAID => static::getPrestaShopOrderStatusId(self::PRESTA_PAYMENT_ACCEPTED)
+                ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_FAILED => static::getPrestaShopOrderStatusId(self::PRESTA_PAYMENT_ERROR)
+                ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_REFUNDED => static::getPrestaShopOrderStatusId(self::PRESTA_REFUNDED)
+                ?? array_values(self::getStatusMap())[0],
             PaymentStates::STATE_PARTIALLY_REFUNDED => static::getPrestaShopOrderStatusId(
-                self::PRESTA_PARTIALLY_REFUNDED
-            ),
-            PaymentStates::STATE_CANCELLED => static::getPrestaShopOrderStatusId(self::PRESTA_CANCELED),
-            PaymentStates::STATE_NEW => static::getPrestaShopOrderStatusId(self::PRESTA_ON_BACKORDER),
+                    self::PRESTA_PARTIALLY_REFUNDED
+                ) ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_CANCELLED => static::getPrestaShopOrderStatusId(self::PRESTA_CANCELED)
+                ?? array_values(self::getStatusMap())[0],
+            PaymentStates::STATE_NEW => static::getPrestaShopOrderStatusId(self::PRESTA_ON_BACKORDER)
+                ?? array_values(self::getStatusMap())[0],
             PaymentStates::CHARGE_BACK => static::getPrestaShopOrderStatusId(self::PRESTA_CHARGEBACK)
+                ?? array_values(self::getStatusMap())[0]
         ];
     }
 
