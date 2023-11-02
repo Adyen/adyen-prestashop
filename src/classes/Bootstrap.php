913817e8366db09d4c431f2a6cd847ee48b57a59
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2023-11-02 12:33:04 +0100

    Update Application info logic and release 5.0.4
    ISSUE: CS-4807

--- src/classes/Bootstrap.php
+++ src/classes/Bootstrap.php
@@ -28,7 +28,6 @@ use Adyen\Core\BusinessLogic\Domain\Integration\Processors\ShopperEmailProcessor
 use Adyen\Core\BusinessLogic\Domain\Integration\Processors\ShopperLocaleProcessor as ShopperLocaleProcessorInterface;
 use Adyen\Core\BusinessLogic\Domain\Integration\Processors\ShopperNameProcessor as ShopperNameProcessorInterface;
 use Adyen\Core\BusinessLogic\Domain\Integration\Processors\ShopperReferenceProcessor as ShopperReferenceProcessorInterface;
-use Adyen\Core\BusinessLogic\Domain\Integration\Processors\ApplicationInfoProcessor as ApplicationInfoProcessorInterface;
 use Adyen\Core\BusinessLogic\Domain\Integration\Store\StoreService as StoreServiceInterface;
 use Adyen\Core\BusinessLogic\Domain\Integration\SystemInfo\SystemInfoService as SystemInfoServiceInterface;
 use Adyen\Core\BusinessLogic\Domain\Integration\Version\VersionService as VersionServiceInterface;
@@ -70,7 +69,6 @@ use AdyenPayment\Classes\Services\Integration\ConfigService;
 use AdyenPayment\Classes\Services\Integration\Logger\LoggerService;
 use AdyenPayment\Classes\Services\Integration\OrderService;
 use AdyenPayment\Classes\Services\Integration\PaymentProcessors\AddressProcessor;
-use AdyenPayment\Classes\Services\Integration\PaymentProcessors\ApplicationInfoProcessor;
 use AdyenPayment\Classes\Services\Integration\PaymentProcessors\BasketItemsProcessor;
 use AdyenPayment\Classes\Services\Integration\PaymentProcessors\BirthdayProcessor;
 use AdyenPayment\Classes\Services\Integration\PaymentProcessors\DeviceFingerprintProcessor;
@@ -335,13 +333,6 @@ class Bootstrap extends BootstrapComponent
             ShopperReferenceProcessorInterface::class,
             static function () {
                 return new ShopperReferenceProcessor();
-            }
-        );
-
-        ServiceRegister::registerService(
-            ApplicationInfoProcessorInterface::class,
-            static function () {
-                return new ApplicationInfoProcessor();
             }
         );
     }
