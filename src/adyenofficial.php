a7d4efaf609cb698f25d917cbc44080f6cdf8aa2
Tamara <tamara@example.com>
2024-07-09 10:26:53 +0200

    Release version 5.1.16
    CS-5644

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -46,7 +46,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '5.1.15';
+        $this->version = '5.1.16';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
