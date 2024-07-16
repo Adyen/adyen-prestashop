1c820c95f5fe3fe90f147014e549f402b70756a5
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-07-16 11:31:48 +0200

    Release version 5.1.17
    ISSUE: CS-5644

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -46,7 +46,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '5.1.16';
+        $this->version = '5.1.17';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
