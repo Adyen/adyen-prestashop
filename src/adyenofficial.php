ece2f6b7d89339a4658b6b15a129c7ba9fd049d2
Filip Kojic <filip.kojic@logeecom.com>
2025-02-03 14:55:31 +0100

    Release version 5.2.9

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -46,7 +46,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '5.2.8';
+        $this->version = '5.2.9';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
