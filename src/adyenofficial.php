8077101adaa80db2beb63def1f9d62f86f1f007a
Marija <marija.ivic@logeecom.com>
2025-04-14 11:05:29 +0200

    Release version 7.1.0
    CS-6784

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -46,7 +46,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '7.0.0';
+        $this->version = '7.1.0';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
