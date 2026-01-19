e88733f672583619f83c11ca8aca25ad9534d242
Boljanovic <aleksandar.boljanovic@logeecom.com>
2026-01-19 13:47:15 +0100

    Release version 7.2.2

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -47,7 +47,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '7.2.1';
+        $this->version = '7.2.2';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
