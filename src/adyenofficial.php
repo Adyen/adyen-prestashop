68bd3965747ccf82e7e8c73e00e03015886ba9aa
Tamara Vukovic <tamara.vukovic@logeecom.com>
2026-04-20 10:37:03 +0200

    Release version 7.4.1

--- src/adyenofficial.php
+++ src/adyenofficial.php
@@ -19,7 +19,7 @@ class AdyenOfficial extends PaymentModule
     {
         $this->name = 'adyenofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '7.4.0';
+        $this->version = '7.4.1';
 
         $this->author = $this->l('Adyen');
         $this->need_instance = 0;
